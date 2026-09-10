ecb0e054ff3de71f91b95c75275b0209772cd6ec
rafal <rafal.szczepkowski@wp.pl>
2026-09-10 02:25:40 +0200

    revert: cofnij synchroniczny XHR - blokowal glowny watek (5s freeze)

--- _i18n.js
+++ _i18n.js
@@ -73,38 +73,28 @@
         document.documentElement.lang = _lang;
     }
 
-    var LOCALE_V = '20260910b';
+    var LOCALE_V = '20260910a';
     function load(lang) {
-        try {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'locales/' + lang + '.json?v=' + LOCALE_V, false);
-            xhr.send(null);
-            if (xhr.status === 200) {
-                _data = JSON.parse(xhr.responseText);
-            } else if (lang !== 'pl') {
-                xhr.open('GET', 'locales/pl.json?v=' + LOCALE_V, false);
-                xhr.send(null);
-                if (xhr.status === 200) _data = JSON.parse(xhr.responseText);
-            }
-        } catch (e) {
-            if (lang !== 'pl') {
-                try {
-                    var xhr2 = new XMLHttpRequest();
-                    xhr2.open('GET', 'locales/pl.json?v=' + LOCALE_V, false);
-                    xhr2.send(null);
-                    if (xhr2.status === 200) _data = JSON.parse(xhr2.responseText);
-                } catch (e2) {}
-            }
-        }
-        _ready = true;
-        if (document.readyState !== 'loading') {
-            applyI18n();
-        } else {
-            document.addEventListener('DOMContentLoaded', applyI18n);
-        }
-        _queue.forEach(function (fn) { fn(); });
-        _queue.length = 0;
+        return fetch('locales/' + lang + '.json?v=' + LOCALE_V)
+            .then(function (r) {
+                if (!r.ok) throw new Error('HTTP ' + r.status);
+                return r.json();
+            })
+            .then(function (data) {
+                _data = data;
+                _ready = true;
+                if (document.readyState !== 'loading') {
+                    applyI18n();
+                } else {
+                    document.addEventListener('DOMContentLoaded', applyI18n);
+                }
+                _queue.forEach(function (fn) { fn(); });
+                _queue.length = 0;
+            })
+            .catch(function () {
+                if (lang !== 'pl') return load('pl');
+            });
     }
 
-    load(_lang);
+    window._i18nReady = load(_lang);
 })();
